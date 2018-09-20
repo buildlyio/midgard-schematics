d2dfb2d1a806a67cf1936909576a3fa4c535187c
Alex Percsi <alex.percsi@humanitec.com>
2018-09-20 08:45:08 +0200

    feat: Clone modules based on walhall application that the package is dropped in chore: Fix eslint errors in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,68 @@
-'use strict';
-
 import gulp from 'gulp';
 import git from 'gulp-git';
 import { spawn } from 'child_process';
+import { readFileSync } from 'fs';
 
-gulp.task('clone', () => {
-  git.clone('git@github.com:Humanitec/midgard-contacts.git', {args: './sub/folder'}, err => {
+const originalPath = process.cwd();
+
+const readConfig = () => {
+  const configPath = originalPath + 'config.js';
+
+  let config;
+
+  try {
+    config = readFileSync(configPath);
+  } catch (e) {
+    throw new Error("Error loading application configuration: " + e.message);
+  }
+
+  try {
+    config = JSON.parse(config);
+  } catch (e) {
+    throw new Error("Error parsing application configuration: " + e.message);
+  }
+
+  return config;
+};
+
+const clone = (url, folderName) => {
+
+  const options = {
+    args: folderName
+  };
+
+  git.clone(url, options, (err) => {
     if (err) {
       throw err;
     }
   });
+};
+
+gulp.task('modules:init', () => {
+  const config = readConfig();
+
+  if (!config) {
+    throw new Error("Application configuration not found");
+  }
+
+  if (!config.modules || !config.modules.length) {
+    throw new Error("No application modules found");
+  }
+ 
+  process.chdir('projects');
+  for (let i = 0; i < config.modules.length; i++) {
+    const module = config.modules[i];
+    if (module.url) {
+      clone(module.url, module.name);
+    }
+  }
+  process.chdir('../');
 });
 
- 
 gulp.task('schematics', (cb) => {
   process.chdir('../');
 
-  let exit = {}
+  const exit = {};
   const child = spawn('ng', ['g', '.:import-module', '--name=contacts']);
 
   child.on('exit', (code, signal) => {
@@ -31,15 +77,14 @@ gulp.task('schematics', (cb) => {
   child.stdout.on('data', (data) => {
     console.log(data.toString());
   });
-  
+
   child.stderr.on('data', (data) => {
     console.error(data.toString());
   });
 
   if (!exit.code) {
     return cb();
-  }
-  else {
-    throw `Schematics child process exited with code ${exit.code}, signal ${exit.signal}`;
+  } else {
+    throw new Error(`Schematics child process exited with code ${exit.code}, signal ${exit.signal}`);
   }
 });
