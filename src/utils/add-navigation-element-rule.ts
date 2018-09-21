3b342859d49cd1fc8f190ac4dc8e9c81056cdfcd
Aziz Haddad <aziz-haddad@users.noreply.github.com>
2018-09-21 17:10:28 +0200

    cheerio parser test

--- src/utils/add-navigation-element-rule.ts
+++ src/utils/add-navigation-element-rule.ts
@@ -1,7 +1,7 @@
-import { Rule, SchematicsException, Tree } from "@angular-devkit/schematics";
+import { Rule, Tree } from "@angular-devkit/schematics";
 import { ModuleOptions } from "@schematics/angular/utility/find-module";
 import { capitalize } from "@angular-devkit/core/src/utils/strings";
-import { JSDOM } from 'jsdom'
+import * as cheerio from 'cheerio'
 
 export function addNavigationElementRule (options: ModuleOptions): Rule {
     return (host: Tree) => {
@@ -9,14 +9,16 @@ export function addNavigationElementRule (options: ModuleOptions): Rule {
         const content: Buffer | null = host.read(navigationPath);
         let strContent: string = '';
         if(content) strContent = content.toString('utf8');
-        const dom = new JSDOM(strContent);
-        const navBarElem = dom.window.document.querySelector(".nav-bar");
-        const contentToInsert = dom.window.document.createElement(`<div class="nav-bar__elem"><a [routerLink]="['/${options.name}']"><i class="fas fa-store"></i>${capitalize(options.name)}</a></div>`);
-        if(!navBarElem || !navBarElem.parentNode) {
-            throw new SchematicsException(`Navbar element is not defined`);
-        }
-        navBarElem.parentNode.insertBefore(contentToInsert, navBarElem.nextSibling);
-        host.overwrite(navigationPath, dom.window.document.body.toString());
+        const cheerioDom = cheerio.load(strContent);
+        const navBarElem = cheerioDom('.nav-bar');
+        const contentToInsert = `<div class="nav-bar__elem">
+            <a [routerLink]="['/${options.name}']">
+                <i class="fas fa-store"></i>
+                ${capitalize(options.name)}
+            </a>
+        </div>`;
+        navBarElem.append(contentToInsert);
+        host.overwrite(navigationPath, navBarElem.toString());
         return host;
     };
 }
