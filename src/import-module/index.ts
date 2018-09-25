d827a0d30ceb88a9a84bcf96b8716a6a402d9456
Aziz Haddad <aziz-haddad@users.noreply.github.com>
2018-09-25 10:58:09 +0200

    add rules to modify json configurations (angular.json and tsconfig.json)

--- src/import-module/index.ts
+++ src/import-module/index.ts
@@ -2,6 +2,7 @@ import { Rule, Tree, SchematicContext, chain} from '@angular-devkit/schematics';
 import { addRouteRule } from "../utils/add-route-utils";
 import { addAddReducersAndEpicsRule } from "../utils/add-reducers-and-epics-utils";
 import { addNavigationElementRule } from "../utils/add-navigation-element-rule";
+import { addLibraryToWorkspaceFileRule, updateTsConfigRule } from "../utils/add-configs-utils";
 
 export function importModule(options: any): Rule {
 
@@ -12,7 +13,9 @@ export function importModule(options: any): Rule {
     const rule = chain([
         addRouteRule(options),
         addAddReducersAndEpicsRule(options),
-        addNavigationElementRule(options)
+        addNavigationElementRule(options),
+        addLibraryToWorkspaceFileRule(options),
+        updateTsConfigRule(options)
         ]);
 
     return rule(host, context);
---
+++ src/utils/add-configs-utils.ts
@@ -0,0 +1,103 @@
+import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import { ModuleOptions } from "@schematics/angular/utility/find-module";
+import { getWorkspace, getWorkspacePath, WorkspaceSchema } from "@schematics/angular/utility/config";
+
+interface UpdateJsonFn<T> {
+    (obj: T): T | void;
+}
+
+type TsConfigPartialType = {
+    compilerOptions: {
+        baseUrl: string,
+        paths: {
+            [key: string]: string[];
+        },
+    },
+    include: string[]
+};
+
+// this function is a utility to update a json file, it is copied from @angular/dev-kit/packages/libray/index.ts
+function updateJsonFile<T>(host: Tree, path: string, callback: UpdateJsonFn<T>): Tree {
+    const source = host.read(path);
+    if (source) {
+        const sourceText = source.toString('utf-8');
+        const json = JSON.parse(sourceText);
+        callback(json);
+        host.overwrite(path, JSON.stringify(json, null, 2));
+    }
+
+    return host;
+}
+
+/**
+ * @description rule to update the application ts config file and add the imported module to the include array
+ * @param {ModuleOptions} options
+ * @returns {Rule}
+ */
+export function updateTsConfigRule (options: ModuleOptions): Rule {
+
+    return (host: Tree) => {
+        if (!host.exists('src/tsconfig.app.json')) { return host; }
+
+        return updateJsonFile(host, 'src/tsconfig.app.json', (tsconfig: TsConfigPartialType) => {
+            if (!tsconfig.include) {
+                tsconfig.include = [];
+            }
+            tsconfig.include.push(`../projects/midgard-angular/src/lib/${options.name}.module.ts`);
+        });
+    };
+}
+
+/**
+ * @description rule to update the application angular.json file and add the imported module to the projects array in the angular workspace
+ * @param {ModuleOptions} options
+ * @returns {Rule}
+ */
+export function addLibraryToWorkspaceFileRule(options: any): Rule {
+    return (host: Tree) => {
+
+        const workspace = getWorkspace(host);
+        const projectRoot = `projects/${options.name}`;
+        // tslint:disable-next-line:no-any
+        const project: any = {
+            root: `${projectRoot}`,
+            projectType: 'library',
+            architect: {
+                build: {
+                    builder: '@angular-devkit/build-ng-packagr:build',
+                    options: {
+                        project: `${projectRoot}/ng-package.json`,
+                    },
+                    configurations: {
+                        production: {
+                            project: `${projectRoot}/ng-package.prod.json`,
+                        },
+                    },
+                },
+                test: {
+                    builder: '@angular-devkit/build-angular:karma',
+                    options: {
+                        main: `${projectRoot}/src/test.ts`,
+                        tsConfig: `${projectRoot}/tsconfig.spec.json`,
+                        karmaConfig: `${projectRoot}/karma.conf.js`,
+                    },
+                },
+                lint: {
+                    builder: '@angular-devkit/build-angular:tslint',
+                    options: {
+                        tsConfig: [
+                            `${projectRoot}/tsconfig.lint.json`,
+                            `${projectRoot}/tsconfig.spec.json`,
+                        ],
+                        exclude: [
+                            '**/node_modules/**',
+                        ],
+                    },
+                },
+            },
+        };
+
+        workspace.projects[options.name] = project;
+        host.overwrite(getWorkspacePath(host), JSON.stringify(workspace, null, 2));
+    };
+}
