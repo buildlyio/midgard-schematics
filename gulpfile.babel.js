d0893a6219ff59ae523d0961e889905a6e1022cb
Alex Percsi <alex.percsi@humanitec.com>
2018-09-28 15:20:50 +0200

    feat: first pass at implementing a full initialization chain (clone -> npm install -> schematics)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 import gulp from 'gulp';
 import git from 'gulp-git';
+import install from 'gulp-install';
 import { spawn } from 'child_process';
 import { readFileSync } from 'fs';
 
@@ -25,21 +26,58 @@ const readConfig = () => {
   return config;
 };
 
+const config = readConfig();
+
 const clone = (url, folderName) => {
   const options = {
     args: folderName
   };
 
-  git.clone(url, options, (err) => {
-    if (err) {
-      console.warn(err.message);
+  return new Promise((resolve, reject) => {
+    git.clone(url, options, (err) => {
+      if (err) {
+        console.warn(err.message);
+        return reject(err);
+      }
+      return resolve();
+    });
+  });
+};
+
+const schematics = (module) => {
+  return new Promise((resolve, reject) => {
+    process.chdir('../../node_modules/midgard_schematics/');
+
+    const exit = {};
+    const child = spawn('ng', ['g', '.:import-module', `--name=${module.name}`]);
+
+    child.on('exit', (code, signal) => {
+      exit.code = code;
+      exit.signal = signal;
+    });
+
+    child.on('error', (err) => {
+      reject(err);
+    });
+
+    child.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    child.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+
+    if (!exit.code) {
+      process.chdir('../../');
+      return resolve();
+    } else {
+      return reject(new Error(`Schematics child process exited with code ${exit.code}, signal ${exit.signal}`));
     }
   });
 };
 
-gulp.task('modules:init', () => {
-  const config = readConfig();
-
+gulp.task('init', () => {
   if (!config) {
     throw new Error('Application configuration not found');
   }
@@ -48,9 +86,28 @@ gulp.task('modules:init', () => {
     throw new Error('No application modules found');
   }
 
-  process.chdir('projects');
+  const tasksToRun = [];
   for (let i = 0; i < config.modules.length; i++) {
     const module = config.modules[i];
+    const taskName = `init:${module.name}`;
+    gulp.task(taskName, () => {
+      process.chdir('projects');
+      return clone(module.url, module.name)
+        .then(() => {
+          process.chdir(module.name);
+          return new Promise((resolve, reject) => {
+            return gulp.src('./package.json')
+              .pipe(install())
+              .on('error', reject)
+              .on('end', resolve);
+          });
+        })
+        .then(schematics(module))
+        .catch(() => {
+
+        });
+    });
+    tasksToRun.push(taskName);
     if (module.url) {
       clone(module.url, module.name);
     } else {
@@ -58,34 +115,4 @@ gulp.task('modules:init', () => {
     }
   }
   process.chdir('../');
-});
-
-gulp.task('schematics', (cb) => {
-  process.chdir('../');
-
-  const exit = {};
-  const child = spawn('ng', ['g', '.:import-module', '--name=contacts']);
-
-  child.on('exit', (code, signal) => {
-    exit.code = code;
-    exit.signal = signal;
-  });
-
-  child.on('error', (err) => {
-    throw err;
-  });
-
-  child.stdout.on('data', (data) => {
-    console.log(data.toString());
-  });
-
-  child.stderr.on('data', (data) => {
-    console.error(data.toString());
-  });
-
-  if (!exit.code) {
-    return cb();
-  } else {
-    throw new Error(`Schematics child process exited with code ${exit.code}, signal ${exit.signal}`);
-  }
 });
