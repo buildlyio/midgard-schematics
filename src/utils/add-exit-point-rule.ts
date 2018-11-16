93eaf3b3b35501daadbba93e48fc42628a114c6e
Aziz Haddad <aziz-haddad@users.noreply.github.com>
2018-11-16 17:16:02 +0100

    modify exit point rule to be dynamic

--- src/utils/add-exit-point-rule.ts
+++ src/utils/add-exit-point-rule.ts
@@ -1,17 +1,15 @@
 import { Rule, Tree } from "@angular-devkit/schematics";
-import { ModuleOptions } from "@schematics/angular/utility/find-module";
-import { capitalize } from "@angular-devkit/core/src/utils/strings";
 import * as cheerio from 'cheerio'
 
-export function addExitPointRule (options: ModuleOptions): Rule {
+export function addExitPointRule (options: any): Rule {
     return (host: Tree) => {
-        const navigationPath = 'projects/midgard-angular/src/lib/components/nav-bar/nav-bar.component.html'; // we can get this from options as well for more flexibility
+        const navigationPath = options.parentExitPointComponent.path; // we can get this from options as well for more flexibility
         const content: Buffer | null = host.read(navigationPath);
         let strContent: string = '';
         if(content) strContent = content.toString('utf8');
         const cheerioDom = cheerio.load(strContent);
         const navBarElem = cheerioDom('.nav-bar');
-        const contentToInsert = `<div class="nav-bar__elem"><a routerLink="/${options.name}"><i class="fas fa-store"></i>${capitalize(options.name)}</a></div>`;
+        const contentToInsert = `<${options.parentExitPointComponent.componentSelector} label="${options.parentExitPointComponent.options.label}"></${options.parentExitPointComponent.componentSelector}>`;
         navBarElem.append(contentToInsert);
         // string content to append to the file
         const newFileContent = navBarElem.toString().replace(new RegExp('routerlink', 'g'), 'routerLink');
