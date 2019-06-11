a112e79f4d9ccb25361289f2c586849041a1f7ab
Aziz <aziz@humanitec.com>
2019-06-11 16:01:05 +0200

    fix exit point rule

--- src/utils/add-exit-point-rule.ts
+++ src/utils/add-exit-point-rule.ts
@@ -30,9 +30,11 @@ function createContentToAdd(options: any): string {
     const route = options.parentExitPointComponentRoute ? `route="${options.parentExitPointComponentRoute}"`: '';
     if (options.parentExitPointComponentSelector) {
         if (options.parentExitPointComponentRouterOutlet) {
-            return `  <${options.parentExitPointComponentSelector} ${label} ${route} ${icon}><router-outlet name="${options.parentExitPointComponentRouterOutlet}"></router-outlet></${options.parentExitPointComponentSelector}>`
+            return `  <${options.parentExitPointComponentSelector} ${label} ${route} ${icon}><router-outlet name="${options.parentExitPointComponentRouterOutlet}"></router-outlet></${options.parentExitPointComponentSelector}>
+`
         } else {
-            return `  <${options.parentExitPointComponentSelector} ${label} ${route} ${icon}></${options.parentExitPointComponentSelector}>`
+            return `  <${options.parentExitPointComponentSelector} ${label} ${route} ${icon}></${options.parentExitPointComponentSelector}>
+`
         }
     }
     else return ''
